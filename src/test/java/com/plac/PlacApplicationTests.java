2b339c361ecdb6c07fcb61b3e0bcb125c9a62c94
matt <matt1235@naver.com>
2023-10-31 18:21:11 +0900

    Initial Commit #4

---
+++ src/test/java/com/plac/PlacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
