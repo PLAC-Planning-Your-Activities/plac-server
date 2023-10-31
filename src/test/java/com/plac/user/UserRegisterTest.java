2a243966f9f516ea9044b7dd0652c9d37ea740e5
matt <matt1235@naver.com>
2023-11-01 03:22:39 +0900

    [Red] 약한 암호면 회원가입 실패하는 테스트 작성 #7

---
+++ src/test/java/com/plac/user/UserRegisterTest.java
@@ -0,0 +1,34 @@
+package com.plac.user;
+
+import com.plac.social_login.exception.WeakPasswordException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserRegisterTest {
+
+    private UserService userService;
+
+    private WeakPasswordChecker mockPasswordChecker = Mockito.mock(WeakPasswordChecker.class);
+
+    @BeforeEach
+    void setUp(){
+        userService = new UserService(mockPasswordChecker);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        BDDMockito.given(mockPasswordChecker.checkWeakPassword("weak-pw"))
+                .willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userService.register("username1@email.com", "weak-pw");
+        });
+    }
+
+}
