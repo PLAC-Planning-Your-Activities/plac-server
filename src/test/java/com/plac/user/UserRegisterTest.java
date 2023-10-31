8d3c46aa1fabbfea618864f89b058ee1d0adc357
matt <matt1235@naver.com>
2023-11-01 03:33:49 +0900

    [Red] username 중복 시, 회원가입이 불가능한 테스트 작성 #7

--- src/test/java/com/plac/user/UserRegisterTest.java
+++ src/test/java/com/plac/user/UserRegisterTest.java
@@ -14,10 +14,12 @@ public class UserRegisterTest {
     private UserService userService;
 
     private WeakPasswordChecker mockPasswordChecker = Mockito.mock(WeakPasswordChecker.class);
+    private UserRepository fakeRepository = new MemoryUserRepository();
 
     @BeforeEach
     void setUp(){
-        userService = new UserService(mockPasswordChecker);
+        userService = new UserService(mockPasswordChecker,
+                fakeRepository);
     }
 
     @DisplayName("약한 암호면 가입 실패")
@@ -28,6 +30,16 @@ public class UserRegisterTest {
 
         assertThrows(WeakPasswordException.class, () -> {
             userService.register("username1@email.com", "weak-pw");
+        });
+    }
+
+    @DisplayName("username(이메일)이 이미 존재하는 경우, 회원가입 불가")
+    @Test
+    void username이_이미_존재하는경우_회원가입_불가(){
+        fakeRepository.save(new User("username1@email.com", "pw123"));
+
+        assertThrows(DuplUsernameException.class, ()->{
+            userService.register("username1@email.com", "pw334");
         });
     }
 
