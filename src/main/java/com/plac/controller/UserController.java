2324f823e1e2221889e3befeeb3daa8849142d91
matt <matt1235@naver.com>
2023-11-11 04:15:30 +0900

    Refactor : UserController 클래스 리팩토링 #9

--- src/main/java/com/plac/controller/UserController.java
+++ src/main/java/com/plac/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.plac.controller;
 
 import com.plac.domain.Message;
-import com.plac.dto.request.UserReqDto;
+import com.plac.dto.request.user.UserReqDto;
+import com.plac.dto.response.user.UserResDto;
 import com.plac.service.UserService;
+import com.plac.util.SecurityContextHolderUtil;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,14 +32,9 @@ public class UserController {
     )
     @PostMapping("/one")
     public ResponseEntity<?> createOne(@RequestBody UserReqDto.CreateUser req) throws Exception {
-
         userService.register(req);
 
-        Message message = Message.builder()
-                .status(HttpStatus.OK)
-                .message("success")
-                .build();
-        return new ResponseEntity<>(message, message.getStatus());
+        return buildResponseEntity(HttpStatus.OK, "success");
     }
 
     @Operation(summary = "유저 삭제 api", description = "유저 하나를 삭제한다.")
@@ -51,9 +50,43 @@ public class UserController {
     public ResponseEntity<?> deleteOne() throws Exception {
         userService.deleteUser();
 
+        return buildResponseEntity(HttpStatus.OK, "success");
+    }
+
+
+    /**
+     * TEST URL : 테스트가 끝나면 필수적으로 지워야 함.
+     * */
+    @Operation(summary = "로그인 테스트 api", description = "로그인 상태에서, 해당 유저의 정보를 출력.")
+    @GetMapping("/test1")
+    public ResponseEntity<?> TestApi() throws Exception {
+        Long userId = SecurityContextHolderUtil.getUserId();
+        UserResDto userResDto = UserResDto.of(userService.findOne(userId));
+
+        return buildResponseEntity(userResDto, HttpStatus.OK, "success");
+    }
+
+    @Operation(summary = "로그인 테스트 api", description = "로그인 상태에서, 등록된 모든 유저정보 출력")
+    @GetMapping("/test2")
+    public ResponseEntity<?> searchAll(){
+        List<UserResDto> result = userService.findAll();
+
+        return buildResponseEntity(result, HttpStatus.OK, "success");
+    }
+
+    private ResponseEntity<Message> buildResponseEntity(HttpStatus status, String msg) {
         Message message = Message.builder()
-                .status(HttpStatus.OK)
-                .message("success")
+                .status(status)
+                .message(msg)
+                .build();
+        return new ResponseEntity<>(message, message.getStatus());
+    }
+
+    private ResponseEntity<Message> buildResponseEntity(Object data, HttpStatus status, String msg) {
+        Message message = Message.builder()
+                .data(data)
+                .status(status)
+                .message(msg)
                 .build();
         return new ResponseEntity<>(message, message.getStatus());
     }
