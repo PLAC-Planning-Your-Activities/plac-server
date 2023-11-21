8fa2ce8a2e13a300d71bc72ce7574f128bf79fc1
matt <matt1235@naver.com>
2023-11-21 22:05:10 +0900

    Refactor : UserServiceImpl 테스트 리팩토링 #17 @BeforeEach 를 통해 반복되는 부분을 묶음

--- src/test/java/com/plac/service/user/UserServiceImplTest.java
+++ src/test/java/com/plac/service/user/UserServiceImplTest.java
@@ -3,9 +3,9 @@ package com.plac.service.user;
 import com.plac.domain.User;
 import com.plac.dto.request.user.UserReqDto;
 import com.plac.dto.response.user.UserResDto;
-import com.plac.repository.RefreshTokenRepository;
 import com.plac.repository.UserRepository;
 import com.plac.service.password_checker.PasswordChecker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,18 +32,41 @@ class UserServiceImplTest {
 
     @Autowired
     private UserService userService;
-
     @MockBean
     private UserRepository userRepository;
-
     @MockBean
     private PasswordChecker passwordChecker;
-
-    @MockBean
-    private RefreshTokenRepository refreshTokenRepository;
-
     @MockBean
     private BCryptPasswordEncoder encoder;
+    private User mockUser;
+    private List<User> mockUserList;
+
+    @BeforeEach
+    void setUp(){
+        Long userId = 1L;
+        mockUser = User.builder()
+                .id(userId)
+                .username("test1@email.com")
+                .provider("normal")
+                .roles("ROLE_USER")
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        mockUserList = Arrays.asList(
+                User.builder()
+                        .username("test2@example.com")
+                        .provider("google")
+                        .roles("ROLE_USER")
+                        .createdAt(LocalDateTime.now())
+                        .build(),
+                User.builder()
+                        .username("test3@example.com")
+                        .provider("kakao")
+                        .roles("ROLE_USER")
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+    }
 
     @Test
     @DisplayName("사용자 등록(회원가입)이 올바르게 동작하는지 테스트")
@@ -51,14 +74,6 @@ class UserServiceImplTest {
         UserReqDto.CreateUser reqDto = new UserReqDto.CreateUser();
         reqDto.setUsername("test1@email.com");
         reqDto.setPassword("password1234");
-
-        User mockUser = User.builder()
-                .username(reqDto.getUsername())
-                .password(reqDto.getPassword())
-                .provider("normal")
-                .roles("ROLE_USER")
-                .createdAt(LocalDateTime.now())
-                .build();
 
         when(userRepository.save(any(User.class))).thenReturn(mockUser);
         when(encoder.encode(anyString())).thenReturn("encodedPassword");
@@ -74,13 +89,6 @@ class UserServiceImplTest {
     @DisplayName("유저 조회 테스트. 특정 사용자 ID로 사용자를 조회할 때, 예상대로 사용자가 반환되는지를 확인하기 위해 작성")
     void findUserTest() {
         Long userId = 1L;
-        User mockUser = User.builder()
-                .id(userId)
-                .username("test2@email.com")
-                .provider("google")
-                .roles("ROLE_USER")
-                .createdAt(LocalDateTime.now())
-                .build();
 
         when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
         User result = userService.findUser(userId);
@@ -93,13 +101,6 @@ class UserServiceImplTest {
     @DisplayName("유저 삭제 테스트 : deleteUser() 호출 시 예외가 발생하지 않음을 확인")
     void deleteUserByIdTest() {
         Long userId = 1L;
-        User mockUser = User.builder()
-                .id(userId)
-                .username("test1@email.com")
-                .provider("naver")
-                .roles("ROLE_USER")
-                .createdAt(LocalDateTime.now())
-                .build();
 
         when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
         doNothing().when(userRepository).delete(mockUser);
@@ -110,22 +111,7 @@ class UserServiceImplTest {
     @Test
     @DisplayName("모든 유저 조회 테스트")
     void findAllUserTest() {
-        List<User> users = Arrays.asList(
-            User.builder()
-                    .username("user1@example.com")
-                    .provider("google")
-                    .roles("ROLE_USER")
-                    .createdAt(LocalDateTime.now())
-                    .build(),
-            User.builder()
-                    .username("user2@example.com")
-                    .provider("kakao")
-                    .roles("ROLE_USER")
-                    .createdAt(LocalDateTime.now())
-                    .build()
-        );
-
-        when(userRepository.findAll()).thenReturn(users);
+        when(userRepository.findAll()).thenReturn(mockUserList);
 
         List<UserResDto> result = userService.findAll();
         assertNotNull(result);
