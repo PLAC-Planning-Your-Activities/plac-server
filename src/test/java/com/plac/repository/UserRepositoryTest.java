8f11dcd95655aab7713f290c8e2b35506c20a0be
matt <matt1235@naver.com>
2023-11-21 22:45:49 +0900

    Refactor : UserRepository 테스트 리팩토링 #17

--- src/test/java/com/plac/repository/UserRepositoryTest.java
+++ src/test/java/com/plac/repository/UserRepositoryTest.java
@@ -27,70 +27,71 @@ public class UserRepositoryTest {
 
     @BeforeEach
     void setUp(){
+        Long userId = 1L;
+
         User buildUser = User.builder()
+                .id(userId)
                 .provider("normal")
-                .username("email1@email.com")
-                .password("pw1234")
+                .username("test1@email.com")
+                .password("password1234")
                 .roles("ROLE_USER")
                 .createdAt(LocalDateTime.now())
                 .build();
-
         userRepository.save(buildUser);
     }
 
     @DisplayName("유저 저장 테스트")
     @Test
     void userSaveTest(){
-        User buildUser = User.builder()
-                .provider("normal")
-                .username("email2@email.com")
-                .password("pw1234")
+        User testUser = User.builder()
+                .provider("google")
+                .username("test2@email.com")
+                .password("password1234")
                 .roles("ROLE_USER")
                 .createdAt(LocalDateTime.now())
                 .build();
+        userRepository.save(testUser);
 
-        userRepository.save(buildUser);
-
-        Optional<User> userOpt = userRepository.findByUsername("email2@email.com");
-        assertEquals(userOpt.get(), buildUser);
+        Optional<User> userOpt = userRepository.findByUsername("test2@email.com");
+        assertEquals(userOpt.get(), testUser);
     }
 
-    @DisplayName("user_id로 찾기 테스트")
+    @DisplayName("user_id로 해당 id를 가지는 유저 조회")
     @Test
     void findByIdTest(){
-        Optional<User> userOpt = userRepository.findById(1L);
-        assertEquals("email1@email.com", userOpt.get().getUsername());
+        Optional<User> userOpt = userRepository.findByUsernameAndProvider("test1@email.com", "normal");
+
+        Optional<User> result = userRepository.findById(userOpt.get().getId());
+        assertEquals(userOpt.get().getId(), result.get().getId());
     }
 
-    @DisplayName("username, provider 로 찾는 테스트")
+    @DisplayName("username, provider 로 유저를 찾을 수 있음")
     @Test
     void findUsernameAndProviderTest(){
-        User buildUser = User.builder()
-                .provider("naver")
-                .username("email3@email.com")
-                .password("pw12345")
-                .roles("ROLE_USER")
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        userRepository.save(buildUser);
-
-        Optional<User> userOpt = userRepository.findByUsernameAndProvider("email3@email.com", "naver");
-        assertEquals("email3@email.com", userOpt.get().getUsername());
+        Optional<User> userOpt = userRepository.findByUsernameAndProvider("test1@email.com", "normal");
+        assertEquals("test1@email.com", userOpt.get().getUsername());
+        assertEquals("normal", userOpt.get().getProvider());
     }
 
     @DisplayName("유저 삭제 테스트")
     @Test
     void deleteTest(){
-        // 기존에 저장된 사용자를 조회
-        Optional<User> userOpt = userRepository.findById(1L);
+        User testUser = User.builder()
+                .provider("naver")
+                .username("naver_user@email.com")
+                .password("password1234")
+                .roles("ROLE_USER")
+                .createdAt(LocalDateTime.now())
+                .build();
+        userRepository.save(testUser);
 
+        Optional<User> userOpt = userRepository.findByUsernameAndProvider("naver_user@email.com", "naver");
         // 사용자가 존재하는 경우에만 테스트 진행
         assertTrue(userOpt.isPresent(), "유저가 존재합니다.");
 
         userRepository.delete(userOpt.get());
 
-        Optional<User> deleteUser = userRepository.findById(1L);
+        Optional<User> deleteUser = userRepository.findByUsernameAndProvider("naver_user", "naver");
         assertFalse(deleteUser.isPresent(), "사용자가 존재하지 않습니다.");
 
     }
