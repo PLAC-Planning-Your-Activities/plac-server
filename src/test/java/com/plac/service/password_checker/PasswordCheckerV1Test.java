adcd977518a09f06b227d2969aa4f49fe97ae7cc
matt <matt1235@naver.com>
2023-11-21 22:10:52 +0900

    Test : PasswordChecker 테스트 구현 #17

---
+++ src/test/java/com/plac/service/password_checker/PasswordCheckerV1Test.java
@@ -0,0 +1,37 @@
+package com.plac.service.password_checker;
+
+import com.plac.exception.user.WeakPasswordException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PasswordCheckerV1Test {
+
+    private PasswordChecker passwordChecker;
+
+    @BeforeEach
+    void setUp(){
+        passwordChecker = new PasswordCheckerV1();
+    }
+
+    @Test
+    @DisplayName("유효한 비밀번호는 예외를 발생시키지 않아야 함")
+    void checkValidPassword() {
+        assertDoesNotThrow(() -> passwordChecker.checkWeakPassword("ValidPass123"));
+    }
+
+    @Test
+    @DisplayName("너무 짧은 비밀번호는 WeakPasswordException을 발생시켜야 함")
+    void checkShortPassword() {
+        assertThrows(WeakPasswordException.class, () -> passwordChecker.checkWeakPassword("123"));
+    }
+
+    @Test
+    @DisplayName("너무 긴 비밀번호는 WeakPasswordException을 발생시켜야 함")
+    void checkLongPassword() {
+        assertThrows(WeakPasswordException.class, () -> passwordChecker.checkWeakPassword("ThisPasswordIsWayTooLong12345"));
+    }
+}
