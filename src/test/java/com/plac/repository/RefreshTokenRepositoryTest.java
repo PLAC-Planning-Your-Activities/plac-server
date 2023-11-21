cc8ecb56b5e5748f0ad25ebfef5d7c608a74513b
matt <matt1235@naver.com>
2023-11-21 23:06:37 +0900

    Test : RefreshTokenRepository 단위 테스트 작성 #17

---
+++ src/test/java/com/plac/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,104 @@
+package com.plac.repository;
+
+import com.plac.domain.RefreshToken;
+import com.plac.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:application-test.yml")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class RefreshTokenRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+    private User buildUser;
+
+    @BeforeEach
+    void setUp(){
+        buildUser = User.builder()
+                .provider("normal")
+                .username("test33@email.com")
+                .password("password1234")
+                .roles("ROLE_USER")
+                .createdAt(LocalDateTime.now())
+                .build();
+        userRepository.save(buildUser);
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .id(UUID.randomUUID())
+                .refreshToken("refreshTokenValue1234")
+                .userId(buildUser.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+        refreshTokenRepository.save(refreshToken);
+    }
+
+    @Test
+    @DisplayName("유저id로 리프레시토큰 반환")
+    void findByUserId() {
+        Optional<User> userOpt = userRepository.findByUsernameAndProvider("test33@email.com", "normal");
+        Optional<RefreshToken> refreshTokenOpt = refreshTokenRepository.findByUserId(userOpt.get().getId());
+
+        assertNotNull(refreshTokenOpt);
+    }
+
+    @Test
+    @DisplayName("id로 리프레시토큰 반환")
+    void findById() {
+        Optional<User> userOpt = userRepository.findByUsernameAndProvider("test33@email.com", "normal");
+        Optional<RefreshToken> refreshTokenOpt = refreshTokenRepository.findByUserId(userOpt.get().getId());
+        UUID refreshTokenId = refreshTokenOpt.get().getId();
+
+        assertEquals(refreshTokenOpt.get(), refreshTokenRepository.findById(refreshTokenId).get());
+    }
+
+    @Test
+    @DisplayName("리프레시 토큰 삭제")
+    void delete(){
+        settings("test123@email.com", "google");
+        Optional<User> userOpt = userRepository.findByUsernameAndProvider("test123@email.com", "google");
+        Long userId = userOpt.get().getId();
+        Optional<RefreshToken> refreshTokenOpt= refreshTokenRepository.findByUserId(userId);
+
+        refreshTokenRepository.delete(refreshTokenOpt.get());
+        assertFalse(refreshTokenRepository.findByUserId(userId).isPresent());
+    }
+
+    private void settings(String username, String provider) {
+        buildUser = User.builder()
+                .provider(provider)
+                .username(username)
+                .password("password1234")
+                .roles("ROLE_USER")
+                .createdAt(LocalDateTime.now())
+                .build();
+        userRepository.save(buildUser);
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .id(UUID.randomUUID())
+                .refreshToken("refreshTokenValue1235")
+                .userId(buildUser.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+        refreshTokenRepository.save(refreshToken);
+    }
+}
