422a3826482a9ac7877a1f5b1f6f61921f0ff2aa
matt <matt1235@naver.com>
2024-01-11 07:42:17 +0900

    chore : Jpaconfig 클래스를 삭제, @EnableJpaAuditing을 Main 에 추가 (#30)

--- src/main/java/com/plac/PlacApplication.java
+++ src/main/java/com/plac/PlacApplication.java
@@ -2,8 +2,10 @@ package com.plac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PlacApplication {
 
 	public static void main(String[] args) {
