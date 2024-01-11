c76bf04f366f46bd1c94f83dc76107470cd1434e
matt <matt1235@naver.com>
2024-01-11 18:23:36 +0900

    feat : 플랜, 플랜-장소 매핑 엔티티 생성 (#34)

--- src/main/java/com/plac/domain/plan/entity/Plan.java
+++ src/main/java/com/plac/domain/plan/entity/Plan.java
@@ -1,7 +1,43 @@
 package com.plac.domain.plan.entity;
 
+import com.plac.common.AbstractTimeEntity;
+import com.plac.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Plan {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Plan extends AbstractTimeEntity {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String thumbnailImage;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private boolean open;
+
+    private String destinationName;
+
+    @Builder
+    public Plan(String name, String thumbnailImage, User user, boolean open, String destinationName) {
+        this.name = name;
+        this.thumbnailImage = thumbnailImage;
+        this.user = user;
+        this.open = open;
+        this.destinationName = destinationName;
+    }
 }
