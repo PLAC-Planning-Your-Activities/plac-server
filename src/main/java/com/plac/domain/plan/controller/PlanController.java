39bb199b556dd629fc43ed7c18ef11830534c3f4
matt <matt1235@naver.com>
2024-01-11 18:24:32 +0900

    feat : 플랜 저장하는 기능 구현 (#34)

--- src/main/java/com/plac/domain/plan/controller/PlanController.java
+++ src/main/java/com/plac/domain/plan/controller/PlanController.java
@@ -1,7 +1,28 @@
 package com.plac.domain.plan.controller;
 
+import com.plac.domain.plan.dto.request.PlanCreateRequest;
+import com.plac.domain.plan.dto.response.PlanCreateResponse;
+import com.plac.domain.plan.service.PlanService;
+import com.plac.util.MessageUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/plans")
 public class PlanController {
+
+    private final PlanService planService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createPlan(PlanCreateRequest planRequest){
+        PlanCreateResponse result = planService.createPlan(planRequest);
+
+        return MessageUtil.buildResponseEntity(result, HttpStatus.OK, "success");
+    }
+
 }
--- src/main/java/com/plac/domain/plan/service/PlanService.java
+++ src/main/java/com/plac/domain/plan/service/PlanService.java
@@ -1,7 +1,79 @@
 package com.plac.domain.plan.service;
 
+import com.plac.domain.place.dto.response.PlaceInfo;
+import com.plac.domain.place.entity.Place;
+import com.plac.domain.place.repository.PlaceRepository;
+import com.plac.domain.plan.dto.request.PlanCreateRequest;
+import com.plac.domain.plan.dto.response.PlanCreateResponse;
+import com.plac.domain.plan.entity.Plan;
+import com.plac.domain.plan.repository.PlanRepository;
+import com.plac.domain.user.entity.User;
+import com.plac.domain.user.repository.UserRepository;
+import com.plac.exception.user.UserPrincipalNotFoundException;
+import com.plac.util.SecurityContextHolderUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class PlanService {
+
+    private final PlaceRepository placeRepository;
+    private final PlanRepository planRepository;
+    private final UserRepository userRepository;
+
+    List<Long> placeIdList = new ArrayList<>();
+
+    public PlanCreateResponse createPlan(PlanCreateRequest planRequest) {
+        Long userId = SecurityContextHolderUtil.getUserId();
+
+        Optional<User> userOpt = userRepository.findById(userId);
+
+        if (!userOpt.isPresent()) {
+            throw new UserPrincipalNotFoundException("no user");
+        }
+        User user = userOpt.get();
+        List<PlaceInfo> placeList = planRequest.getPlaceList();
+
+        for (PlaceInfo placeInfo : placeList) {
+            placeRepository.findByKakaoPlaceId(placeInfo.getKakaoPlaceId())
+                    .ifPresent(place -> placeIdList.add(createAndSavePlace(place)));
+        }
+
+        Plan savedPlan = createNewPlan(planRequest, user);
+        return new PlanCreateResponse(savedPlan.getId());
+    }
+
+    private Plan createNewPlan(PlanCreateRequest planRequest, User user) {
+        Plan plan = Plan.builder()
+                .destinationName(planRequest.getDestinationName())
+                .name(planRequest.getPlanName())
+                .user(user)
+                .open(false)
+                .build();
+
+        Plan savedPlan = planRepository.save(plan);
+
+        return savedPlan;
+    }
+
+    private Long createAndSavePlace(Place place) {
+        Place newPlace = Place.builder()
+                .placeName(place.getPlaceName())
+                .type(place.getType())
+                .kakaoPlaceId(place.getKakaoPlaceId())
+                .x(place.getX())
+                .y(place.getY())
+                .thumbnailImageUrl(place.getThumbnailImageUrl())
+                .streetNameAddress(place.getStreetNameAddress())
+                .build();
+
+        Place savePlace = placeRepository.save(newPlace);
+
+        return savePlace.getId();
+    }
 }
