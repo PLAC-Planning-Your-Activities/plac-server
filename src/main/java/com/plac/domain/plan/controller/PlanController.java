c389945b1d46a5c5645262048088c2486c7f0e72
matt <matt1235@naver.com>
2024-01-11 18:40:20 +0900

    feat : 플랜 커뮤니티에 공유하는 기능 구현 (#34)

--- src/main/java/com/plac/domain/plan/controller/PlanController.java
+++ src/main/java/com/plac/domain/plan/controller/PlanController.java
@@ -1,15 +1,14 @@
 package com.plac.domain.plan.controller;
 
 import com.plac.domain.plan.dto.request.PlanCreateRequest;
+import com.plac.domain.plan.dto.request.PlanShareRequest;
 import com.plac.domain.plan.dto.response.PlanCreateResponse;
 import com.plac.domain.plan.service.PlanService;
 import com.plac.util.MessageUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +18,20 @@ public class PlanController {
     private final PlanService planService;
 
     @PostMapping("")
-    public ResponseEntity<?> createPlan(PlanCreateRequest planRequest){
+    public ResponseEntity<?> createPlan(@RequestBody PlanCreateRequest planRequest){
         PlanCreateResponse result = planService.createPlan(planRequest);
 
         return MessageUtil.buildResponseEntity(result, HttpStatus.OK, "success");
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> sharePlanToCommunity(
+            @RequestBody PlanShareRequest planRequest,
+            @PathVariable("planId") Long planId
+    ){
+        planService.sharePlanToCommunity(planRequest, planId);
+
+        return MessageUtil.buildResponseEntity(HttpStatus.OK, "success");
     }
 
 }
