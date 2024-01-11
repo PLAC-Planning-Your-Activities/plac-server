f17fe855240004beb9fb2e87e6ddb571d67a99e7
matt <matt1235@naver.com>
2024-01-12 06:42:49 +0900

    feat : 목적지에 따른 플랜을 보여주는 기능 구현 (#34)

--- src/main/java/com/plac/domain/place/dto/response/PlaceInfo.java
+++ src/main/java/com/plac/domain/place/dto/response/PlaceInfo.java
@@ -1,5 +1,6 @@
 package com.plac.domain.place.dto.response;
 
+import com.plac.domain.place.entity.Place;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -16,4 +17,17 @@ public class PlaceInfo {
     private BigDecimal x;
     private BigDecimal y;
     private String address;
+    private int page;
+
+    public static PlaceInfo of(Place place){
+        return PlaceInfo.builder()
+                .placeName(place.getPlaceName())
+                .type(place.getType())
+                .kakaoPlaceId(place.getKakaoPlaceId())
+                .imageUrl(place.getThumbnailImageUrl())
+                .x(place.getX())
+                .y(place.getY())
+                .address(place.getStreetNameAddress())
+                .build();
+    }
 }
