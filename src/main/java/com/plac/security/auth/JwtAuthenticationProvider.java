5a7e30e99b5b01d0d65f619da3736f68879a6963
Big-Cir97 <eodnjs01477@gmail.com>
2024-03-08 16:31:00 +0900

    refactor: 불필요 코드 제거

--- src/main/java/com/plac/security/auth/JwtAuthenticationProvider.java
+++ src/main/java/com/plac/security/auth/JwtAuthenticationProvider.java
@@ -29,8 +29,6 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         UUID salt = savedUser.getUser().getSalt();
 
         String saltedPassword = password + salt.toString();
-        System.out.println(saltedPassword);
-        System.out.println(savedUser.getPassword());
 
         if(!passwordEncoder.matches(saltedPassword, savedUser.getPassword())) {
             throw new BadCredentialsException("로그인 정보가 올바르지 않습니다.");
