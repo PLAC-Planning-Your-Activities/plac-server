5195a7d46bd232af8bb6a7ac4d7fc996d0c88768
matt <matt1235@naver.com>
2024-03-13 14:52:00 +0900

    refactor: Message 클래스 리팩토링 (#44) Message 클래스의 data 필드를 제외하고 모두 삭제했습니다.

--- src/main/java/com/plac/common/Message.java
+++ src/main/java/com/plac/common/Message.java
@@ -1,23 +1,13 @@
 package com.plac.common;
 
 import lombok.*;
-import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
-@ToString
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class Message {
     private Object data;
-    private HttpStatus status;
-    private int code;
-    private String message;
 
-    public Message(String message, int code, HttpStatus status) {
-        this.message = message;
-        this.code = code;
-        this.status = status;
+    public Message(Object data) {
+        this.data = data;
     }
 }
