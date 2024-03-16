a9717728f0520756a8ba688f35248113a66a80ea
Big-Cir97 <eodnjs01477@gmail.com>
2024-03-17 01:25:40 +0900

    feat: planDibs repository 생성

---
+++ src/main/java/com/plac/domain/plan/repository/planDibs/PlanDibsQueryRepository.java
@@ -0,0 +1,4 @@
+package com.plac.domain.plan.repository.planDibs;
+
+public interface PlanDibsQueryRepository {
+}
---
+++ src/main/java/com/plac/domain/plan/repository/planDibs/PlanDibsQueryRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.plac.domain.plan.repository.planDibs;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlanDibsQueryRepositoryImpl implements PlanDibsQueryRepository {
+}
