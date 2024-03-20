27f7b5c404956c6156dc872ce44738615d5e3849
matt <matt1235@naver.com>
2024-03-20 21:52:55 +0900

    feat: 커뮤니티에서 플랜 조회 QueryDSL 작성 (#54)

--- src/main/java/com/plac/domain/plan/repository/plan/PlanQueryRepositoryImpl.java
+++ src/main/java/com/plac/domain/plan/repository/plan/PlanQueryRepositoryImpl.java
@@ -1,13 +1,24 @@
 package com.plac.domain.plan.repository.plan;
 
 import com.plac.domain.plan.entity.Plan;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.plac.domain.place.entity.QPlace.place;
+import static com.plac.domain.plan.entity.QBookmarkPlan.bookmarkPlan;
 import static com.plac.domain.plan.entity.QPlan.plan;
 import static com.plac.domain.plan.entity.QPlanDibs.planDibs;
+import static com.plac.domain.plan.entity.QPlanPlaceMapping.planPlaceMapping;
+import static com.plac.domain.plan.entity.QPlanTag.planTag;
+import static com.plac.domain.plan.entity.QPlanTagMapping.planTagMapping;
+import static com.plac.domain.user.entity.QUser.user;
 
 @Repository
 public class PlanQueryRepositoryImpl implements PlanQueryRepository {
@@ -34,4 +45,47 @@ public class PlanQueryRepositoryImpl implements PlanQueryRepository {
                 .orderBy(planDibs.createdAt.desc())
                 .fetch();
     }
+
+    public List<Plan> findFilteredPlan(String destinationName, String placeName, String sortBy, int ageRange,
+                                       String gender, List<String> tags, long tagCount, Pageable pageable) {
+        JPAQuery<Plan> query = jpaQueryFactory.selectFrom(plan)
+                .join(plan.user, user)
+                .leftJoin(planPlaceMapping).on(plan.id.eq(planPlaceMapping.plan.id))
+                .leftJoin(planPlaceMapping.place, place)
+                .leftJoin(planDibs).on(plan.id.eq(planDibs.planId))
+                .leftJoin(bookmarkPlan).on(plan.id.eq(bookmarkPlan.plan.id))
+                .leftJoin(planTagMapping).on(plan.id.eq(planTagMapping.plan.id))
+                .leftJoin(planTagMapping.planTag, planTag);
+
+        BooleanExpression whereCondition = plan.destinationName.eq(destinationName)
+                .and(user.ageRange.eq(ageRange))
+                .and(user.gender.eq(gender));
+
+        if (placeName != null && !placeName.isEmpty()) {
+            whereCondition = whereCondition.and(place.placeName.like(placeName));
+        }
+
+        if (tags != null && !tags.isEmpty()) {
+            whereCondition = whereCondition.and(planTag.tagName.in(tags))
+                    .and(planTagMapping.planTag.id.countDistinct().eq(tagCount));
+        }
+
+        query.where(whereCondition);
+
+        OrderSpecifier<?> orderBy = new OrderSpecifier<>(Order.DESC, plan.createdAt);
+        if ("인기순".equals(sortBy)) {
+            orderBy = new OrderSpecifier<>(Order.DESC, planDibs.count());
+        } else if ("저장순".equals(sortBy)) {
+            orderBy = new OrderSpecifier<>(Order.DESC, bookmarkPlan.count());
+        }
+
+        // Apply paging, fetch
+        List<Plan> results = query.orderBy(orderBy)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return results;
+    }
+
 }
--- src/main/java/com/plac/domain/plan/repository/plan/PlanRepository.java
+++ src/main/java/com/plac/domain/plan/repository/plan/PlanRepository.java
@@ -3,10 +3,10 @@ package com.plac.domain.plan.repository.plan;
 import com.plac.domain.plan.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface PlanRepository extends JpaRepository <Plan, Long>, PlanQueryRepository {
 
     Optional<Plan> findByName(String name);
+
 }
