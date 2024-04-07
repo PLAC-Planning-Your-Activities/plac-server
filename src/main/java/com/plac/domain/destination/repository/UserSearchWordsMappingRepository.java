a59fad2ac0b41a36e0539a16b2225d0f1d29b438
matt <matt1235@naver.com>
2024-04-07 19:13:57 +0900

    fix: 인기 검색 Top6 버그 수정

--- src/main/java/com/plac/domain/destination/repository/UserSearchWordsMappingRepository.java
+++ src/main/java/com/plac/domain/destination/repository/UserSearchWordsMappingRepository.java
@@ -10,19 +10,19 @@ import java.util.List;
 
 public interface UserSearchWordsMappingRepository extends JpaRepository<UserSearchWords, Long> {
 
-    @Query("SELECT searchWords.name, COUNT(searchWords) as frequency " +
+    @Query("SELECT searchWords.name " +
             "FROM UserSearchWords searchWordsMapping " +
             "JOIN searchWordsMapping.user user " +
             "JOIN searchWordsMapping.searchWords searchWords " +
             "WHERE user.ageGroup = :ageGroup AND user.gender = :gender " +
             "GROUP BY searchWords.name " +
-            "ORDER BY frequency DESC")
+            "ORDER BY COUNT(searchWords) DESC")
     List<String> findTop6SearchWordsByAgeGroupAndGender(@Param("ageGroup") int ageGroup, @Param("gender") String gender, Pageable pageable);
 
-    @Query("SELECT searchWords.name, COUNT(searchWords) as frequency " +
+    @Query("SELECT searchWords.name " +
             "FROM UserSearchWords searchWordsMapping " +
             "JOIN searchWordsMapping.searchWords searchWords " +
             "GROUP BY searchWords " +
-            "ORDER BY frequency DESC")
+            "ORDER BY COUNT(searchWords)")
     List<String> findTop6SearchWords(Pageable pageable);
 }
